baa67d81060854a319619299cbcd2ed2b800e13a
Hugo Sobral <hugosobral10@gmail.com>
2020-01-14 00:12:29 -0300

    creates initial structure of forms

--- app/controllers/router.py
+++ app/controllers/router.py
@@ -1,10 +1,19 @@
 from flask import render_template
 from app import app
 
+from app.models.forms import loginForm
+
 @app.route("/", methods=['GET'])
 def index():
-    return render_template('index.html', user=name)
+    return render_template('index.html')
 
-@app.route("/login")
+@app.route("/login", methods=["POST", "GET"])
 def login():
-    return render_template('base.html')
+    form = loginForm()
+    if form.validate_on_submit():
+        print(form.username.data)
+        print(form.password.data)
+    else:
+        print(form.errors)
+
+    return render_template('login.html', form=form)
