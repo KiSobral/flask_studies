387445d401b4ba0a42ff4e6d3bfa13186c06073c
Hugo Sobral <hugosobral10@gmail.com>
2020-01-15 18:24:29 -0300

    fix db config bugs

--- app/controllers/router.py
+++ app/controllers/router.py
@@ -1,6 +1,7 @@
 from flask import render_template
-from app import app
+from app import app, db
 
+from app.models.tables import User
 from app.models.forms import loginForm
 
 @app.route("/", methods=['GET'])
