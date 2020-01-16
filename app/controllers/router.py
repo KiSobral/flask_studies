01d45884cb7e4d2455943b9d718c0db7a3824010
Hugo Sobral <hugosobral10@gmail.com>
2020-01-15 22:21:51 -0300

    Handle login permissions in application

--- app/controllers/router.py
+++ app/controllers/router.py
@@ -1,8 +1,13 @@
-from flask import render_template
-from app import app, db
+from flask import render_template, flash, redirect, url_for
+from flask_login import login_user, logout_user
+from app import app, db, login_manager
 
 from app.models.tables import User
 from app.models.forms import loginForm
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.filter_by(id=id).first()
 
 @app.route("/", methods=['GET'])
 def index():
@@ -12,9 +17,21 @@ def index():
 def login():
     form = loginForm()
     if form.validate_on_submit():
-        print(form.username.data)
-        print(form.password.data)
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and user.password == form.password.data:
+            login_user(user)
+            flash("Logged succesfully.")
+            return redirect(url_for("index"))
+        else:
+            flash("Invalid login.")
+    
     else:
         print(form.errors)
 
     return render_template('login.html', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    flash("Logged out succesfully.")
+    return redirect(url_for("index"))
